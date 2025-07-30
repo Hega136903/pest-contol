9cca2d3dcee362f1e720a014b1e4bb9ba4820338
Hega136903 <visvishwa6903@gmail.com>
2025-07-30 19:05:20 +0530

    Add files via upload

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -4,7 +4,7 @@ const BookingSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   mobile: { type: String, required: true },
   address: { type: String, required: true },
-  service: { type: String, enum: ['Ant Control', 'Termite Control', 'Cockroach Control'], required: true },
+  service: { type: String, enum: ['Rodent Control', 'Termite Control', 'Cockroach Control'], required: true },
   preferredDate: { type: Date, required: true },
   preferredTime: { type: String, required: true },
   completed: { type: Boolean, default: false },
